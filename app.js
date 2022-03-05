51453dbfd96cf8ec813ec0153b78888b98a41918
Hubert Rozplochowski <Hubert.Rozplochowski@createctrl.com>
2022-03-05 16:12:18 +0100

    Basic routing set up

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-const myApp = angular.module('myApp', ['ngRoute']);
+const myApp = angular.module('myApp', ['ngRoute', 'ngResource']);
+
+myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    console.log('CONFIG')
+    $routeProvider
+        .when('/', { 
+            templateUrl: 'views/main.html',
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html',
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+
+    $locationProvider.html5Mode(true); 
+    $locationProvider.hashPrefix('');
+}]);
